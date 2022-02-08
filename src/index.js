574f96a2f8a83bd4d0b7a2436eb2077a70b746f7
Ausdauer1 <steady051@naver.com>
2022-02-08 18:07:41 +0900

    16개 통과 성공했다잉

--- src/index.js
+++ src/index.js
@@ -1,10 +1,83 @@
-class Site {}
+class Site {
+    constructor() {
+        this.boards = [];
+    }
 
-class Board {}
+    addBoard(newBoard) {
+        const existboardname = this.boards.find((i) => i.name === newBoard.name);
+        if (!existboardname) {
+            newBoard.siteBoard = true;
+            this.boards.push(newBoard);
+        } else {
+            throw new Error('에헤이~ 틀렸다잉~ 돌아가');
+        }
+    }
 
-class Article {}
+    findBoardByName(findBoard) {
+        return this.boards.find((i) => i.name === findBoard);
+    }
+}
 
-class Comment {}
+class Board {
+    constructor(boardName) {
+        if (boardName === '' || boardName === null) {
+            throw new Error('에헤이~ 틀렸다잉~ 돌아가');
+        } else {
+            this.name = boardName;
+            this.siteBoard = false;
+            this.articles = [];
+        }
+    }
+    publish(article) {
+        if (this.siteBoard === false) {
+            throw new Error('에헤이~ 틀렸다잉~ 돌아가');
+        } else {
+            article.id = `${this.name}-${Math.random()}`;
+            const date = new Date();
+            article.createdDate = date.toISOString();
+            article.boardArticle = true;
+            this.articles.push(article);
+        }
+    }
+    getAllArticles() {
+        return this.articles
+    }
+}
+
+class Article {
+    constructor(articleElements) {
+        if (
+            articleElements.subject === ('' || null) ||
+            articleElements.content === ('' || null) ||
+            articleElements.author === ('' || null)
+        ) {
+            throw new Error('에헤이~ 틀렸다잉~ 돌아가');
+        } else {
+            this.comments = [];
+            this.boardArticle = false;
+        }
+    }
+    reply(comment) {
+        if (this.boardArticle === false) {
+            throw new Error('에헤이~ 틀렸다잉~ 돌아가');
+        } else {
+            const date = new Date();
+            comment.createdDate = date.toISOString();
+            this.comments.push(comment)
+        }
+    }
+    getAllComments() {
+        return this.comments
+    }
+}
+
+class Comment {
+    constructor(comment) {
+        if (comment.content === ('' || null) || comment.author === ('' || null)) {
+            throw new Error('에헤이~ 틀렸다잉~ 돌아가');
+        } 
+    }
+}
 
 module.exports = {
     Site,
